2f5436da5ec8ceae749d1887610e2e7908665115
mkondratov28 <max.kondratov@ibs-banking.com>
2022-09-26 15:43:20 +0200

    localStorage implementation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { StorageService } from '../storage.service';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 import { DialogOpenerComponent } from '../dialog-opener/dialog-opener.component';
@@ -15,33 +16,48 @@ export class HeroesComponent implements OnInit {
 
   dialogOpener = new DialogOpenerComponent(this.dialog);
 
-  constructor(private heroService: HeroService, public dialog: MatDialog) { }
+  constructor(
+    private storageService: StorageService,
+    private heroService: HeroService,
+    public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    // this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroes = this.sortHeroes(this.storageService.getHeroes())
   }
 
   add(name: string): void {
     name = name.trim();
     if (!name) { return; }
-    this.heroService.addHero({ name } as Hero)
-      .subscribe(hero => {
-        this.heroes.push(hero);
-      });
+    // this.heroService.addHero({ name } as Hero)
+    //   .subscribe(hero => {
+    //     this.heroes.push(hero);
+    //   });
+    this.storageService.addHero(name);
+    this.getHeroes();
   }
 
   delete(hero: Hero): void {
     this.heroes = this.heroes.filter(h => h !== hero);
-    this.heroService.deleteHero(hero.id).subscribe();
+    // this.heroService.deleteHero(hero.id).subscribe();
+    this.storageService.deleteHero(hero.id);
+  }
+
+  reset() {
+    this.storageService.resetHeroes();
   }
 
   openDialog(hero: Hero): void {
     this.dialogOpener.openDialog(hero);
     console.log(hero);
+  }
+
+  sortHeroes(heroes: Hero[]): Hero[] {
+    return this.storageService.sortHeroes(heroes);
   }
 
 }
