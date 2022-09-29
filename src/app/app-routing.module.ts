5f0c93c113d808c15329c48e9a2dca05d1d593a3
mkondratov28 <max.kondratov@ibs-banking.com>
2022-09-29 12:08:04 +0200

    hero-leasing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroLeasingComponent } from './hero-leasing/hero-leasing.component';
 
 const routes: Routes = [
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'leasing', component: HeroLeasingComponent },
 ];
 
 @NgModule({
---
+++ src/app/hero-leasing/hero-leasing.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { LeasedHero } from '../leasedHero';
+
+@Component({
+  selector: 'app-hero-leasing',
+  templateUrl: './hero-leasing.component.html',
+  styleUrls: ['./hero-leasing.component.css']
+})
+export class HeroLeasingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
