a946cb5d945169bbadb5849c964c52c91128f520
Marlena <marlena.michalowska@ibs-banking.com>
2022-09-29 13:56:02 +0200

    hero-detail.component

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -33,6 +33,8 @@ export class HeroDetailComponent implements OnInit {
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+
+
   }
 
   getDialog(): void {
